6b4a7cd444fb717d569ef136ee01c1d4fdd8341d
Raghuram-ReddyK <raghuramireddykarnati@gmail.com>
2025-07-01 21:31:17 +0530

    Initial Project upload with all files

---
+++ capstone/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Commentstable from './TableComponents/Commentstable';
+
+function App() {
+  return (
+    <div className="App">
+     <Commentstable/>
+    </div>
+  );
+}
+
+export default App;
---
+++ capstone/src/TableComponents/Commentstable.tsx
@@ -0,0 +1,12 @@
+
+import React from 'react'
+
+const Commentstable = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Commentstable
